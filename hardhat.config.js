999fd27ad67c1af1f027e2d4ed7b8c9959137940
TopCodeBeast <maxb47163@gmail.com>
2024-02-21 06:34:09 -0500

    add polygon mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,41 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
-const accounts = require('../accounts');
- 
+
+const accounts = require("./accounts");
+
 const networks = {
   bsc: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: [accounts.bsc.privateKey]
+    url: "https://bsc-dataseed.binance.org/",
+    chainId: 56,
+    gasPrice: 20000000000,
+    accounts: [accounts.bsc.privateKey],
   },
   test: {
     url: "https://bsc-testnet.web3api.com/v1/KBR2FY9IJ2IXESQMQ45X76BNWDAW2TT3Z3",
     chainId: 97,
     gasPrice: 20000000000,
-    accounts: [accounts.bsc.privateKey]
-  }
+    accounts: [accounts.bsc.privateKey],
+  },
+  mumbai: {
+    url: "https://bsc-testnet.web3api.com/v1/KBR2FY9IJ2IXESQMQ45X76BNWDAW2TT3Z3",
+    chainId: 80001,
+    gasPrice: 20000000000,
+    accounts: [accounts.polygon.privateKey],
+  },
 };
 
 module.exports = {
   solidity: {
-        version: "0.8.13",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200
-            }
-        }
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
+  },
   networks: networks,
   etherscan: {
-    apiKey: "EYK2X8KUEV48N8J3WPJKE5YTY3IHSVJH32"
-  }
+    apiKey: "EYK2X8KUEV48N8J3WPJKE5YTY3IHSVJH32",
+  },
 };
